e1f90888cb38d8aacc177f4158f848aa7a3bf322
LizaImmax <123055305+LizaImmax@users.noreply.github.com>
2023-06-13 09:41:44 -0700

    add item

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,18 +1,17 @@
 #!/usr/bin/python3
-"""Module 9-add_item.
-Adds all arguments to a Python list,
-and then save them to a file.
-"""
+"""Add all arguments to a Python list and save them to a file."""
 
 
-def save_to_json_file(my_obj, filename):
-    """Writes an Object to a text file, using a JSON representation.
+import sys
 
-    Args:
-        my_obj (any): object to be serialized
+if __name__ == "__main__":
+    save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+    load_from_json_file = \
+        __import__('6-load_from_json_file').load_from_json_file
 
-    """
-    import json
-
-    with open(filename, 'w', encoding='utf-8') as file:
-        json.dump(my_obj, file)
+    try:
+        items = load_from_json_file("add_item.json")
+    except FileNotFoundError:
+        items = []
+    items.extend(sys.argv[1:])
+    save_to_json_file(items, "add_item.json")
