cad328c47cf895efd084c20624f0808668b0142e
LizaImmax <123055305+LizaImmax@users.noreply.github.com>
2023-06-13 09:19:08 -0700

    additem

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -4,22 +4,15 @@ Adds all arguments to a Python list,
 and then save them to a file.
 """
 
-import sys
-import json
-import os.path
 
-save_to_json_file = __import__("5-save_to_json_file").save_to_json_file
-load_from_json_file = __import__("6-load_from_json_file").load_from_json_file
+def save_to_json_file(my_obj, filename):
+    """Writes an Object to a text file, using a JSON representation.
 
-my_file = 'add_item.json'
+    Args:
+        my_obj (any): object to be serialized
 
-my_list = []
+    """
+    import json
 
-if os.path.exists(my_file) and os.path.getsize(my_file) > 0:
-    my_list = load_from_json_file(my_file)
-
-if len(sys.argv) > 1:
-    for elem in sys.argv[1:]:
-        my_list.append(elem)
-
-save_to_json_file(my_list, my_file)
+    with open(filename, 'w', encoding='utf-8') as file:
+        json.dump(my_obj, file)
